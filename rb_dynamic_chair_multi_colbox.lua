6bd90c07db9917fb15aee614d4258ffaa2c59f3f
Astrofra <astrofra@gmail.com>
2022-09-05 17:57:58 +0200

    Added more chairs to the chair test.

--- rb_dynamic_chair_multi_colbox.lua
+++ rb_dynamic_chair_multi_colbox.lua
@@ -44,29 +44,29 @@ cube_size =  hg.Vec3(1, 1, 1)
 cube_ref = res:AddModel('cube', hg.CreateCubeModel(vtx_layout, cube_size.x, cube_size.y, cube_size.z))
 
 -- ground
-ground_size = hg.Vec3(15, 0.05, 15)
+ground_size = hg.Vec3(50, 0.05, 50)
 ground_ref = res:AddModel('ground', hg.CreateCubeModel(vtx_layout, ground_size.x, ground_size.y, ground_size.z))
 
 -- setup the scene
 scene = hg.Scene()
 
-cam_mat = hg.TransformationMat4(hg.Vec3(-2, 6, -15.5), hg.Vec3(hg.Deg(15), 0, 0))
-cam = hg.CreateCamera(scene, cam_mat, 0.01, 1000, hg.Deg(35))
+cam_mat = hg.TransformationMat4(hg.Vec3(0, 6, -15.5) * 2.0, hg.Vec3(hg.Deg(15), 0, 0))
+cam = hg.CreateCamera(scene, cam_mat, 0.01, 1000, hg.Deg(30))
 view_matrix = hg.InverseFast(cam_mat)
 c = cam:GetCamera()
 projection_matrix = hg.ComputePerspectiveProjectionMatrix(c:GetZNear(), c:GetZFar(), hg.FovToZoomFactor(c:GetFov()), hg.Vec2(res_x / res_y, 1))
 
 scene:SetCurrentCamera(cam)	
 
-lgt = hg.CreateLinearLight(scene, hg.TransformationMat4(hg.Vec3(0, 0, 0), hg.Vec3(hg.Deg(30), hg.Deg(30), 0)), hg.Color(1, 1, 1), hg.Color(1, 1, 1), 10, hg.LST_Map, 0.00025, hg.Vec4(10, 15, 20, 25))
+lgt = hg.CreateLinearLight(scene, hg.TransformationMat4(hg.Vec3(0, 0, 0), hg.Vec3(hg.Deg(30), hg.Deg(30), 0)), hg.Color(1, 1, 1), hg.Color(1, 1, 1), 10, hg.LST_Map, 0.00075, hg.Vec4(20, 34, 55, 70))
 
-chair_node, _ = hg.CreateInstanceFromAssets(scene, hg.TranslationMat4(hg.Vec3(0, 1, 0)), "chair/chair.scn", res, hg.GetForwardPipelineInfo())
+-- chair_node, _ = hg.CreateInstanceFromAssets(scene, hg.TranslationMat4(hg.Vec3(0, 1, 0)), "chair/chair.scn", res, hg.GetForwardPipelineInfo())
 
-for i = 1, 10 do
+for i = 1, 200 do
     hg.CreateInstanceFromAssets(scene, hg.TranslationMat4(hg.Vec3(0, 1 + i * 5, 0)), "chair/chair.scn", res, hg.GetForwardPipelineInfo())
 end
 
-floor, rb_floor = CreatePhysicCubeEx(scene, ground_size, hg.TranslationMat4(hg.Vec3(-2, -0.005, 0)), ground_ref, {mat_grey}, hg.RBT_Static, 0)
+floor, rb_floor = CreatePhysicCubeEx(scene, ground_size, hg.TranslationMat4(hg.Vec3(0, -0.005, 0)), ground_ref, {mat_grey}, hg.RBT_Static, 0)
 rb_floor:SetRestitution(1)
 
 -- scene physics
@@ -87,17 +87,17 @@ keyboard = hg.Keyboard()
 while not keyboard:Down(hg.K_Escape) and hg.IsWindowOpen(win) do
     keyboard:Update()
 
-    physics:NodeWake(chair_node)
+    -- physics:NodeWake(chair_node)
     view_id = 0
     hg.SceneUpdateSystems(scene, clocks, dt_frame_step, physics, physics_step, 3)
     view_id, pass_id = hg.SubmitSceneToPipeline(view_id, scene, hg.IntRect(0, 0, res_x, res_y), true, pipeline, res)
 
-    -- Debug physics display
-    hg.SetViewClear(view_id, 0, 0, 1.0, 0)
-    hg.SetViewRect(view_id, 0, 0, res_x, res_y)
-    hg.SetViewTransform(view_id, view_matrix, projection_matrix)
-    rs = hg.ComputeRenderState(hg.BM_Opaque, hg.DT_Disabled, hg.FC_Disabled)
-    physics:RenderCollision(view_id, vtx_line_layout, line_shader, rs, 0)
+    -- -- Debug physics display
+    -- hg.SetViewClear(view_id, 0, 0, 1.0, 0)
+    -- hg.SetViewRect(view_id, 0, 0, res_x, res_y)
+    -- hg.SetViewTransform(view_id, view_matrix, projection_matrix)
+    -- rs = hg.ComputeRenderState(hg.BM_Opaque, hg.DT_Disabled, hg.FC_Disabled)
+    -- physics:RenderCollision(view_id, vtx_line_layout, line_shader, rs, 0)
 
     hg.Frame()
     hg.UpdateWindow(win)

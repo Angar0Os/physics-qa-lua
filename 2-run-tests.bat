a132ae590238bff414d8bf16a84f6aa4539adc39
astrofra <astrofra@gmail.com>
2022-11-06 11:18:05 +0100

    Harfang's binary update.

---
+++ 2-run-tests.bat
@@ -0,0 +1,25 @@
+
+bin\lua.exe !rb_dynamic_collision_events.lua
+bin\lua.exe raycast_various_collshapes.lua 
+bin\lua.exe rb_dynamic_add_force_during_update.lua
+bin\lua.exe rb_dynamic_add_force_on_init.lua
+bin\lua.exe rb_dynamic_add_torque_on_init.lua
+bin\lua.exe rb_dynamic_angular_velocity_on_init.lua
+bin\lua.exe rb_dynamic_chair_multi_colbox.lua
+bin\lua.exe rb_dynamic_linear_velocity_on_init.lua
+bin\lua.exe rb_dynamic_variable_friction.lua
+bin\lua.exe rb_dynamic_variable_restitution.lua
+bin\lua.exe rb_dynamic_variable_rolling_friction.lua
+bin\lua.exe rb_kinematic_add_force_during_update.lua
+bin\lua.exe rb_kinematic_add_torque_during_update.lua
+bin\lua.exe rb_kinematic_animation_instance.lua
+bin\lua.exe rb_kinematic_parenting.lua
+bin\lua.exe rb_kinematic_set_position_rotation.lua
+bin\lua.exe rb_kinematic_set_position_rotation_instance.lua
+bin\lua.exe rb_kinematic_set_world_matrix.lua
+bin\lua.exe rb_mesh_collider_raycast.lua
+bin\lua.exe rb_rings_chain.lua
+bin\lua.exe rb_static_reset_position_rotation.lua
+bin\lua.exe rb_static_reset_world_matrix.lua
+bin\lua.exe rb_static_set_position_rotation.lua
+pause
